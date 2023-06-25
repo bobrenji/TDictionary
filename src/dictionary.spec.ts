d02e63ab2195990cba60931045f59267c5f9d167
Renji <renji129@gmail.com>
2023-06-25 21:05:55 +0800

    Possible scenarios

---
+++ src/dictionary.spec.ts
@@ -0,0 +1,10 @@
+describe('Dictionary', function () {
+    it.todo("should add an item to dictionary with key")
+    it.todo("should throw exception if try to add new value with same key")
+    it.todo("should return the size of the dictionary")
+    it.todo("should get value by key")
+    it.todo("should possible to delete item by key")
+    it.todo("should possible to delete all item by calling removeAll")
+    it.todo("should return all key registered in current dictionary")
+    it.todo("should return all values in dictionary")
+});
