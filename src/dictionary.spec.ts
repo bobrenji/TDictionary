56fbf9cb5e61ab3f38c57880305cb46a3ace17a7
Renji <renji129@gmail.com>
2023-06-26 09:52:17 +0800

    Removed todo's from test

--- src/dictionary.spec.ts
+++ src/dictionary.spec.ts
@@ -1,15 +1,6 @@
 import Dictionary from "./dictionary";
 
 describe('Dictionary', function () {
-    it.todo("should add an item to dictionary with key")
-    it.todo("should throw exception if try to add new value with same key")
-    it.todo("should return the size of the dictionary")
-    it.todo("should get value by key")
-    it.todo("should possible to delete item by key")
-    it.todo("should possible to delete all item by calling removeAll")
-    it.todo("should return all keys in current dictionary")
-    it.todo("should return all values in dictionary")
-
     const aDict: Dictionary = new Dictionary()
     beforeEach(() => {
         aDict.removeAll()
