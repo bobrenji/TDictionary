01be5d20a0bea12b5d10bd489ffe89624b857ffe
Renji <renji129@gmail.com>
2023-06-26 08:33:24 +0800

    Scenario: Add item to dict as key-value pair

--- src/dictionary.spec.ts
+++ src/dictionary.spec.ts
@@ -1,3 +1,16 @@
+class Dictionary {
+    private container: { [index: string]: string } = {};
+    private length: number = 0;
+    add(key: string, value: string) {
+        this.container[key] = value;
+        this.length +=1;
+    }
+
+    size(): number {
+        return this.length
+    }
+}
+
 describe('Dictionary', function () {
     it.todo("should add an item to dictionary with key")
     it.todo("should throw exception if try to add new value with same key")
@@ -7,4 +20,18 @@ describe('Dictionary', function () {
     it.todo("should possible to delete all item by calling removeAll")
     it.todo("should return all key registered in current dictionary")
     it.todo("should return all values in dictionary")
+
+    it('should add an item to dictionary with key', function () {
+        const aDict = new Dictionary()
+        aDict.add("name","Renji");
+        expect(aDict.size()).toEqual(1);
+    });
+
+    it('should return the size of the dictionary', function () {
+        const aDict = new Dictionary()
+        aDict.add("firstName","Renji");
+        aDict.add("lastName","Raj");
+        expect(aDict.size()).toEqual(2);
+    });
+
 });
