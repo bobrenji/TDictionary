57d46b04181be87fce850d7a07577e9e06572455
Renji <renji129@gmail.com>
2023-06-26 08:58:11 +0800

    Refactor test code by cleaning the dictionary before every test case executes

--- src/dictionary.spec.ts
+++ src/dictionary.spec.ts
@@ -10,24 +10,25 @@ describe('Dictionary', function () {
     it.todo("should return all key registered in current dictionary")
     it.todo("should return all values in dictionary")
 
+    const aDict = new Dictionary()
+    beforeEach(() => {
+        aDict.removeAll()
+    })
+
     it('should add an item to dictionary with key', function () {
-        const aDict = new Dictionary()
         aDict.add("name","Renji");
         expect(aDict.size()).toEqual(1);
     });
 
     it('should return the size of the dictionary', function () {
-        const aDict = new Dictionary()
         aDict.add("firstName","Renji");
         aDict.add("lastName","Raj");
         expect(aDict.size()).toEqual(2);
     });
 
     it('should possible to delete all item by calling removeAll', function () {
-        const aDict = new Dictionary()
         aDict.add("firstName","Renji");
         aDict.add("lastName","Raj");
-
         aDict.removeAll()
         expect(aDict.size()).toEqual(0);
     });
