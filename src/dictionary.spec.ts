574055d4c1127c45abcf6a1d69a7182171033e07
Renji <renji129@gmail.com>
2023-06-27 14:50:05 +0800

    Handle different type values

--- src/dictionary.spec.ts
+++ src/dictionary.spec.ts
@@ -5,72 +5,87 @@ describe('Dictionary', function () {
     it.todo("Handle value field as generic")
     it.todo("Refactor dictionary class to generic")
 
-    const aDict: Dictionary = new Dictionary()
-    beforeEach(() => {
-        aDict.removeAll()
-    })
+    describe('Dictionary for string', function () {
+        const aDict: Dictionary = new Dictionary()
+        beforeEach(() => {
+            aDict.removeAll()
+        })
 
-    it('should add an item to dictionary with key', function () {
-        aDict.add("name","Renji");
-        expect(aDict.size()).toEqual(1);
+        it('should add an item to dictionary with key', function () {
+            aDict.add("name","Renji");
+            expect(aDict.size()).toEqual(1);
+        });
+
+        it('should return the size of the dictionary', function () {
+            aDict.add("firstName","Renji");
+            aDict.add("lastName","Raj");
+            expect(aDict.size()).toEqual(2);
+        });
+
+        it('should possible to delete all item by calling removeAll', function () {
+            aDict.add("firstName","Renji");
+            aDict.add("lastName","Raj");
+            aDict.removeAll()
+            expect(aDict.size()).toEqual(0);
+        });
+
+        it('should throw exception if try to add new value with same key', function () {
+            aDict.add("firstName", "Renji")
+            expect(() => aDict.add("firstName", "Bob")).toThrowError("UNIQUE ONLY ALLOWED")
+        });
+
+        it('should return undefined if not found for a specific key', function () {
+            expect(aDict.get("lastName")).toBeUndefined()
+        });
+
+        it('should get value by key', function () {
+            aDict.add("firstName", "Bob")
+            expect(aDict.get("firstName")).toEqual("Bob")
+        });
+
+        it('should possible to delete item by key', function () {
+            aDict.add("firstName", "Bob")
+            aDict.add("lastName", "Jack")
+            expect(aDict.size()).toEqual(2)
+            aDict.remove("lastName")
+            expect(aDict.size()).toEqual(1)
+        });
+
+        it('should be the same size if ask to delete an unknown key', function () {
+            aDict.add("firstName", "Bob")
+            expect(aDict.size()).toEqual(1)
+            aDict.remove("lastName")
+            expect(aDict.size()).toEqual(1)
+        });
+
+        it('should return all keys in current dictionary', function () {
+            aDict.add("firstName", "Bob")
+            aDict.add("lastName", "Jack")
+            aDict.add("middleName", "Dani")
+            const keys: string[] = aDict.getAllKeys();
+            expect(keys.length).toEqual(3);
+        });
+
+        it('should return all values in dictionary', function () {
+            aDict.add("firstName", "Bob")
+            aDict.add("lastName", "Jack")
+            aDict.add("middleName", "Dani")
+
+            const values = aDict.getAllValues();
+            expect(values.length).toEqual(3)
+        });
     });
 
-    it('should return the size of the dictionary', function () {
-        aDict.add("firstName","Renji");
-        aDict.add("lastName","Raj");
-        expect(aDict.size()).toEqual(2);
-    });
+    describe('Dictionary for Numbers', function () {
+        const aDict: Dictionary = new Dictionary()
+        beforeEach(() => {
+            aDict.removeAll()
+        })
 
-    it('should possible to delete all item by calling removeAll', function () {
-        aDict.add("firstName","Renji");
-        aDict.add("lastName","Raj");
-        aDict.removeAll()
-        expect(aDict.size()).toEqual(0);
-    });
+        it('should add an item to dictionary with key', function () {
+            aDict.add("income",1000);
+            expect(aDict.size()).toEqual(1);
+        });
 
-    it('should throw exception if try to add new value with same key', function () {
-        aDict.add("firstName", "Renji")
-        expect(() => aDict.add("firstName", "Bob")).toThrowError("UNIQUE ONLY ALLOWED")
-    });
-
-    it('should return undefined if not found for a specific key', function () {
-        expect(aDict.get("lastName")).toBeUndefined()
-    });
-
-    it('should get value by key', function () {
-        aDict.add("firstName", "Bob")
-        expect(aDict.get("firstName")).toEqual("Bob")
-    });
-
-    it('should possible to delete item by key', function () {
-        aDict.add("firstName", "Bob")
-        aDict.add("lastName", "Jack")
-        expect(aDict.size()).toEqual(2)
-        aDict.remove("lastName")
-        expect(aDict.size()).toEqual(1)
-    });
-
-    it('should be the same size if ask to delete an unknown key', function () {
-        aDict.add("firstName", "Bob")
-        expect(aDict.size()).toEqual(1)
-        aDict.remove("lastName")
-        expect(aDict.size()).toEqual(1)
-    });
-
-    it('should return all keys in current dictionary', function () {
-        aDict.add("firstName", "Bob")
-        aDict.add("lastName", "Jack")
-        aDict.add("middleName", "Dani")
-        const keys: string[] = aDict.getAllKeys();
-        expect(keys.length).toEqual(3);
-    });
-
-    it('should return all values in dictionary', function () {
-        aDict.add("firstName", "Bob")
-        aDict.add("lastName", "Jack")
-        aDict.add("middleName", "Dani")
-
-        const values = aDict.getAllValues();
-        expect(values.length).toEqual(3)
     });
 });
